77d465fe719cc7c2019d8b6d237d62d516c0a224
jtraut <jaketraut@gmail.com>
2014-09-17 10:00:13 -0600

    Update hello.java

--- Dropbox/workspace2/lab4/src/lab4/hello.java
+++ Dropbox/workspace2/lab4/src/lab4/hello.java
@@ -3,5 +3,7 @@ package lab4;
 public class hello {
 	public static void main(String[] args){
 		System.out.println("Hello World!"); //Display the string.
+		System.out.println("How are you?");
+		
 	}
 }
